a85b2321af67b99d33a60eccad762832e1afffb3
lghtsT <121576981+lghtsT@users.noreply.github.com>
2025-07-04 18:26:32 +0300

    Add files via upload

--- Урок6 задание3.py
+++ Урок6 задание3.py
@@ -1,9 +1,13 @@
 a = int(input())#инвестиции
 b = int(input())#Иван
 c = int(input())#Майкл
-if (a < b) and (a < c):
+if (a <= b) and (a <= c):
     print(2)
-elif (a < b) or (a < c) or (a < (b + c)):
+elif (a <= b):
+    print("Ivan")
+elif (a <= c):
+    print("Mike")
+elif (a <= (b + c)):
     print(1)
 else:
     print(0)
